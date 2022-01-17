467a9851f1085362dcb15b13de6ffb5ffc37c479
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-01-17 17:48:39 -0300

    Adicionando projeto Interface

---
+++ Intro c#_net/Interface/Interface/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interface
+{
+    interface IPessoa
+    {
+        void Nome(string nome);
+        void Sobrenome(string sobrenome);
+        void Cpf(int cpf);
+    }
+
+    class Pessoa : IPessoa
+    {
+        public string? Nome { get; set; }
+        public string? Sobrenome { get; set; }
+        public int Cpf { get; set; }
+
+        void IPessoa.Nome(string nome)
+        {
+            this.Nome = nome;
+
+        }
+
+        void IPessoa.Sobrenome(string sobrenome)
+        {
+            this.Sobrenome = sobrenome;
+        }
+
+        void IPessoa.Cpf(int cpf)
+        {
+            this.Cpf = cpf;
+        }
+    }
+}
