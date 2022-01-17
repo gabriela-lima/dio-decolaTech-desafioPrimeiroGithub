63d82e7c3bb09423d845ec288fb47d1c04cbe84b
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-01-17 14:18:21 -0300

    Adicionando projeto Contas_Teste

---
+++ Conhecendo o Visual Studio/Contas_Teste/Contas_Teste/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+static void Main(string[] args)
+{
+
+
+}
+
+
+namespace Contas
+{
+    public class Contas
+    {
+        public int Soma(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtracao(int a, int b)
+        {
+            return (a - b);
+        }
+        public int Divisao(int a, int b)
+        {
+            return a / b;
+        }
+        public int Multiplicacao(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
+
+
---
+++ Conhecendo o Visual Studio/Contas_Teste/Contas_TesteTests/ContasTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Contas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contas.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void SomaTest()
+        {
+            var a = 1;
+            var b = 1;
+            var esperado = 2;
+            var contas = new Contas();
+
+            var resultado = contas.Soma(a, b);
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void SubtracaoTest()
+        {
+            var a = 1;
+            var b = 1;
+            var esperado = 0;
+            var contas = new Contas();
+
+            var resultado = contas.Subtracao(a, b);
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void DivisaoTest()
+        {
+            var a = 4;
+            var b = 2;
+            var esperado = 2;
+            var contas = new Contas();
+
+            var resultado = contas.Divisao(a, b);
+            Assert.AreEqual(esperado, resultado);
+        }
+
+        [TestMethod()]
+        public void MultiplicacaoTest()
+        {
+            var a = 1;
+            var b = 2;
+            var esperado = 2;
+            var contas = new Contas();
+
+            var resultado = contas.Multiplicacao(a, b);
+            Assert.AreEqual(esperado, resultado);
+        }
+    }
+}
