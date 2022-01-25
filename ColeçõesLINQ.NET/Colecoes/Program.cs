37b109ff63e28a9f1f9d47b4c4bb9bd4e8b72d45
gabriela-lima <gabrielalima.bcc@gmail.com>
2022-01-25 01:53:11 -0300

    Adicionando o resto dos codigos sobre colecoes

--- ColeçõesLINQ.NET/Colecoes/Program.cs
+++ ColeçõesLINQ.NET/Colecoes/Program.cs
@@ -8,16 +8,123 @@ namespace Colecoes
     {
         static void Main(string[] args)
         {
-            OperacoesLista opLista = new OperacoesLista(); 
+            // //Mostrando LINQ
+            // int[] arrayNumeros = new int[10]{100, 1, 4, 0, 8, 15, 19, 19, 4, 100};
+            // //Pegando o valor minimo, maximo e medio do array
+            // var minimo = arrayNumeros.Min();
+            // var maximo = arrayNumeros.Max();
+            // var medio = arrayNumeros.Average();
+            // //Somar todos os elementos presentes em uma colecao
+            // var soma = arrayNumeros.Sum();
+            // //retornar uma colecao com valores unicos. sem valores repetidos
+            // var arrayUnico = arrayNumeros.Distinct().ToArray();
+            // System.Console.WriteLine($"Minimo: {minimo}");
+            // System.Console.WriteLine($"Maximo: {maximo}");
+            // System.Console.WriteLine($"Medio: {medio}");
+            // System.Console.WriteLine($"Soma: {soma}");
+            // System.Console.WriteLine($"Array Original: {string.Join(", ", arrayNumeros)}");
+            // System.Console.WriteLine($"Array Distinto: {string.Join(", ", arrayUnico)}");
+            // //Usando a sintaxe
+            // var numerosParesQuery =
+            //     from num in arrayNumeros
+            //     where num % 2 == 0
+            //     orderby num
+            //     select num;     
+            // //Usando metodos
+            // var numerosParesMetodos = arrayNumeros.Where(x => x % 2 == 0).OrderBy(x => x).ToList();   
+            // System.Console.WriteLine("Numeros pares query: " + string.Join(", ", numerosParesQuery));     
+            // System.Console.WriteLine("Numeros pares metodo: " + string.Join(", ", numerosParesMetodos));  
+
+            // // //Mostrando dicionario
+            // // //Declarando um dicionario
+            // Dictionary<string, string> estadosD = new Dictionary<string, string>(); 
+            // // //Adicionando elementos
+            // // //deve passar a chave e o valor
+            // // //OBS: nao eh possivel adicionar uma chave igual, duplicada, mas eh possivel repetir o valor
+            // estadosD.Add("SP", "São Paulo");
+            // estadosD.Add("MG", "Minas Gerais");
+            // estadosD.Add("BA", "Bahia");
+            // // //Percorrendo um dicionario
+            // foreach (KeyValuePair<string, string> item in estadosD)
+            // {
+            //     System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+            // }
+            // string valorProcuradoD = "BA";
+            // //Acessando valores de forma segura
+            // //se a chave nao for encontrada pode acontecer um erro, dessa forma ele primeiro tenta achar a chave e se achar continua
+            // if(estadosD.TryGetValue(valorProcuradoD, out string estadoEncontrado))
+            // {
+            //     System.Console.WriteLine(estadoEncontrado);
+            // }
+            // else
+            // {
+            //     System.Console.WriteLine($"Chave {valorProcuradoD} não existe no dicionario");
+            // }
+            // //Removendo um valor
+            // System.Console.WriteLine($"Removendo o valor: {valorProcuradoD}");
+            // estadosD.Remove(valorProcuradoD);
+            // foreach (KeyValuePair<string, string> item in estadosD)
+            // {
+            //     System.Console.WriteLine($"Chave: {item.Key}, Valor: {item.Value}");
+            // }
+            // //Acessando um valor
+            // System.Console.WriteLine(estadosD[valorProcuradoD]);
+            // //Atualizando um valor
+            // System.Console.WriteLine($"Valor original: {estadosD[valorProcuradoD]}");
+            // estadosD[valorProcuradoD] = "BA - Teste atualização";
+            // System.Console.WriteLine($"Valor atualizado: {estadosD[valorProcuradoD]}");
+
+            // //Mostrando pilha
+            // //declaracao de pilha
+            // Stack<string> pilhaLivros = new Stack<string>();
+            // //Adicionando elementos a pilha
+            // pilhaLivros.Push(".NET");
+            // pilhaLivros.Push("DDD");
+            // pilhaLivros.Push("Código limpo");
+
+            // System.Console.WriteLine($"Livros para a leitura: {pilhaLivros.Count}");
+            // //Ler a pilha e ir removendo seus elementos
+            // while(pilhaLivros.Count > 0)
+            // {
+            //     //retorna o livro que esta no final da pilha, mas nao o remove, apenas para saber quem eh o proximo
+            //     System.Console.WriteLine($"Próximo livro para a leitura: {pilhaLivros.Peek()}");
+            //     //pega o ultimo elemento da pilha, o retorna e o remove da pilha
+            //     System.Console.WriteLine($"{pilhaLivros.Pop()} lido com sucesso");
+            // }
+
+            // //Mostrando filas
+            // //declaracao de fila
+            // Queue<string> fila = new Queue<string>();
+            // //Adicionando elementos a fila
+            // //metodo enqueue adiciona um objeto no fim da sua fila
+            // //OBS: nao eh possivel adicionar um elemento em determinado indice, pois a fila segue o padrao FIFO
+            // fila.Enqueue("Leonardo");
+            // fila.Enqueue("Eduardo");
+            // fila.Enqueue("André");
+
+            // System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
+
+            // //para ler a fila e ir removendo os elementos
+            // while(fila.Count > 0)
+            // {
+            //     //retorna a pessoa que esta no comeco da fila, mas nao a remove, apenas para saber quem eh o proximo
+            //     System.Console.WriteLine($"Vez de: {fila.Peek()}");
+            //     //pega o primeiro elemento da fila, o retorna e o remove da fila
+            //     System.Console.WriteLine($"{fila.Dequeue()} atendido");
+            // }
+            // System.Console.WriteLine($"Pessoas na fila: {fila.Count}");
+
+            //Mostrando listas
+            //OperacoesLista opLista = new OperacoesLista(); 
 
             //Mostrando como declarar e acessar uma lista
             //Declaracao de lista
             //nao precisamos passar o tamanho da lista, nao precisamos nos preocupar em manipular o tamanho da lista
-            List<string> estados = new List<string>();
+            //List<string> estados = new List<string>();
             //Adicionando elementos a nossa lista
-            estados.Add("SP");
-            estados.Add("MG");
-            estados.Add("BA");
+            //estados.Add("SP");
+            //estados.Add("MG");
+            //estados.Add("BA");
             //Acessando elementos da lista
             //Metodo Count retorna a quantidade de elementos contidos na lista
             //System.Console.WriteLine($"Quantidade de elementos na lista: {estados.Count}");
@@ -33,7 +140,7 @@ namespace Colecoes
             // {
             //     System.Console.WriteLine($"Indice {i}, valor: {estados[i]}");
             // }
-            opLista.ImprimirListaString(estados);
+            //opLista.ImprimirListaString(estados);
 
             //Remover um elemento da lista
             // System.Console.WriteLine("Removendo o elemento");
@@ -42,12 +149,12 @@ namespace Colecoes
 
             //Mostrando como adicionar elementos de uma colecao a outra colecao sem perder os elementos originais
             //Nesse caso um eh array e o outro eh uma lista
-            string[] estadosArray = new string[2] {"SC", "MT"};
+            //string[] estadosArray = new string[2] {"SC", "MT"};
             //metodo AddRange vai manter os elementos originais e adicionar os outros ao final da lista
             //estados.AddRange(estadosArray);
             //Metodo Insert se voce quiser adicionar um novo elemento em um indice especifico
-            estados.Insert(1, "RJ");
-            opLista.ImprimirListaString(estados);
+            //estados.Insert(1, "RJ");
+            //opLista.ImprimirListaString(estados);
 
 
             // OperacoesArray op = new OperacoesArray();
